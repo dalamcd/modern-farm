fc910cbf832bfe3f44b828d22a89b7f9dd337a51
dalamcd <dalamcd@gmail.com>
2020-10-12 14:58:32 -0500

    Sorted plants alphabetically and displayed group count rather than individually

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
 import { createPlan } from "./plan.js"
-import { usePlants } from "./field.js"
+import { sortPlants, usePlants } from "./field.js"
 import { plantSeeds } from "./tractor.js";
 import { harvestPlants } from "./harvester.js"
 import { Catalog } from "./catalog.js";
 
 const yearlyPlan = createPlan();
 plantSeeds(yearlyPlan);
+sortPlants();
 const plantedField = usePlants();
 const plantYield = harvestPlants(plantedField);
 Catalog(plantYield);
